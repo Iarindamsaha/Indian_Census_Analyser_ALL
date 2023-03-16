d810148aaf6c9b30190f0b43b613e29a55f36717
Arindam Saha <asaha15071998@gmail.com>
2023-03-17 00:36:17 +0530

    Incorrect Delimiter Check

--- src/test/java/org/bridgelabz/CensusAnalyserTestFile.java
+++ src/test/java/org/bridgelabz/CensusAnalyserTestFile.java
@@ -3,12 +3,12 @@ package org.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.CENSUS_FILE_PROBLEM;
-import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;
+import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.*;
 
 public class CensusAnalyserTestFile {
 
     private static final String INCORRECT_FILE_FORMAT = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\main\\resources\\State.txt";
+    private static final String CSV_WITH_WRONG_DELIMITER = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\test\\resources\\IncorrectDelimiterFile.csv";
     private  final String WRONG_CSV_FILE_PATH = "State.csv";
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\main\\resources\\StateCensusData.csv";
 
@@ -40,6 +40,15 @@ public class CensusAnalyserTestFile {
             censusAnalyser.loadIndiaCensusData(INCORRECT_FILE_FORMAT);
         });
         Assertions.assertEquals(CENSUS_INCORRECT_FILE_FORMAT, exceptionRule.type);
+    }
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenCustomDelimiter_ShouldThrowException() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        CensusAnalyzerCustomException exceptionRule = Assertions.assertThrows(CensusAnalyzerCustomException.class,()->{
+            censusAnalyser.loadIndiaCensusData(CSV_WITH_WRONG_DELIMITER);
+        });
+        Assertions.assertEquals(CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER, exceptionRule.type);
     }
 
 }
