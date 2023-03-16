33941098e7b48aaa1da08ec224ec9b09bd00cd16
Arindam Saha <asaha15071998@gmail.com>
2023-03-16 22:25:17 +0530

    Wrong CSV File Path Checked

--- src/test/java/org/bridgelabz/CensusAnalyserTestFile.java
+++ src/test/java/org/bridgelabz/CensusAnalyserTestFile.java
@@ -3,8 +3,11 @@ package org.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.CENSUS_FILE_PROBLEM;
+
 public class CensusAnalyserTestFile {
 
+    private static final String WRONG_CSV_FILE_PATH = "State.csv";
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\main\\resources\\StateCensusData.csv";
 
     //TC 1.1
@@ -14,5 +17,14 @@ public class CensusAnalyserTestFile {
         int count = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_CSV_FILE_PATH);
         Assertions.assertEquals(29, count);
 
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowExceptionSadTest() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        CensusAnalyzerCustomException exceptionRule = Assertions.assertThrows(CensusAnalyzerCustomException.class,()->{
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        });
+        Assertions.assertEquals(CENSUS_FILE_PROBLEM, exceptionRule.type);
     }
 }
