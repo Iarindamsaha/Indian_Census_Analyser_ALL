f53a0b2618abbcd907780529a9b2c2cf1ec0acd3
Arindam Saha <asaha15071998@gmail.com>
2023-03-17 00:59:31 +0530

    Wrong Header Check

--- src/main/java/org/bridgelabz/CensusAnalyser.java
+++ src/main/java/org/bridgelabz/CensusAnalyser.java
@@ -2,7 +2,6 @@ package org.bridgelabz;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -11,28 +10,38 @@ import java.util.Iterator;
 
 public class CensusAnalyser {
 
+
+
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyzerCustomException {
 
-        try {
+        if(!csvFilePath.contains(".txt")){
 
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<IndianCensusAnalyserCSVFile> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(IndianCensusAnalyserCSVFile.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<IndianCensusAnalyserCSVFile> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndianCensusAnalyserCSVFile> censusCSVIterator = csvToBean.iterator();
+            try {
 
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndianCensusAnalyserCSVFile censusData = censusCSVIterator.next();
+                Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+                CsvToBeanBuilder<IndianCensusAnalyserCSVFile> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+                csvToBeanBuilder.withType(IndianCensusAnalyserCSVFile.class);
+                csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+                CsvToBean<IndianCensusAnalyserCSVFile> csvToBean = csvToBeanBuilder.build();
+                Iterator<IndianCensusAnalyserCSVFile> censusCSVIterator = csvToBean.iterator();
+
+                int namOfEateries = 0;
+                while (censusCSVIterator.hasNext()) {
+                    namOfEateries++;
+                    IndianCensusAnalyserCSVFile censusData = censusCSVIterator.next();
+                }
+                return namOfEateries;
+            } catch (IOException e) {
+                throw new CensusAnalyzerCustomException(e.getMessage(), CensusAnalyzerCustomException.ExceptionType.CENSUS_FILE_PROBLEM);
+            } catch(RuntimeException e) {
+                throw new CensusAnalyzerCustomException("CSV File Must Have Comma As Delimiter Has Incorrect Header", CensusAnalyzerCustomException.ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
             }
-            return namOfEateries;
-        } catch (IOException e) {
-            throw new CensusAnalyzerCustomException(e.getMessage(), CensusAnalyzerCustomException.ExceptionType.CENSUS_FILE_PROBLEM);
+
         }
-        catch(RuntimeException e) {
-            throw new CensusAnalyzerCustomException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", CensusAnalyzerCustomException.ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
+        else {
+            throw new CensusAnalyzerCustomException("File Not Supported", CensusAnalyzerCustomException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT);
         }
     }
+
+
 }
