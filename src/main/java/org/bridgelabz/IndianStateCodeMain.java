45faf3d81bddbc51369e9c8c32a8d3b3c81805e5
Arindam Saha <asaha15071998@gmail.com>
2023-03-17 01:23:29 +0530

    Added Indian State Code Program

---
+++ src/main/java/org/bridgelabz/IndianStateCodeMain.java
@@ -0,0 +1,20 @@
+package org.bridgelabz;
+
+public class IndianStateCodeMain {
+
+    public static void main(String[] args) {
+
+        StateCode stateCode = new StateCode();
+        try{
+
+            stateCode.loadIndianStateCode("C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\main\\resources\\StateCode.csv");
+
+        }
+        catch (StateCodeCustomException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
