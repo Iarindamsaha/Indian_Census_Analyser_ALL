086c5e9b6b8dc83a61b8c5895edf7d1bf96812b4
Arindam Saha <asaha15071998@gmail.com>
2023-03-17 01:44:48 +0530

    Testing Wrong File Format

--- src/test/java/org/bridgelabz/StateCodeTestFile.java
+++ src/test/java/org/bridgelabz/StateCodeTestFile.java
@@ -4,14 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.CENSUS_FILE_PROBLEM;
+import static org.bridgelabz.CensusAnalyzerCustomException.ExceptionType.CENSUS_INCORRECT_FILE_FORMAT;
 import static org.bridgelabz.StateCodeCustomException.ExceptionType.STATE_CODE_FILE_PROBLEM;
+import static org.bridgelabz.StateCodeCustomException.ExceptionType.STATE_CODE_INCORRECT_FILE_FORMAT;
 
 public class StateCodeTestFile {
 
     private static final String INDIAN_STATE_CODE_FILE = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\test\\resources\\StateCode.csv";
     private static final String WRONG_STATE_CODE_FILE = "Census.csv";
+    private static final String INCORRECT_STATE_CODE_FILE_FORMAT = "C:\\Users\\asaha\\Desktop\\Java_Fellowship_242\\Indian_States_Census_Analyser_Problem_ALL\\src\\test\\resources\\StateCode.txt";
 
-    //TC 2.2
+    //TC 2.1
     @Test
     public void givenIndianStateCodeFile_WhenLoad_ShouldReturnCorrectRecords() throws StateCodeCustomException {
 
@@ -21,6 +24,7 @@ public class StateCodeTestFile {
 
     }
 
+    //TC 2.2
     @Test
     public void givenIndiaStateData_WithWrongFile_ShouldThrowExceptionSadTest() {
 
@@ -31,5 +35,18 @@ public class StateCodeTestFile {
 
         Assertions.assertEquals(STATE_CODE_FILE_PROBLEM, exceptionRule.type);
     }
+
+    //TC 2.3
+    @Test
+    public void givenIndiaStateCodeData_WithWrongFileFormat_ShouldThrowExceptionSadTest() {
+
+        StateCode stateCode = new StateCode();
+        StateCodeCustomException exceptionRule = Assertions.assertThrows(StateCodeCustomException.class,()->{
+            stateCode.loadIndianStateCode(INCORRECT_STATE_CODE_FILE_FORMAT);
+        });
+
+        Assertions.assertEquals(STATE_CODE_INCORRECT_FILE_FORMAT, exceptionRule.type);
+    }
+
 
 }
