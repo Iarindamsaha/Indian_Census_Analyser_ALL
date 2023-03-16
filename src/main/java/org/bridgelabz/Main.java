30f1c9b846be9a3b5f27f769cc113dffea3af24f
Arindam Saha <asaha15071998@gmail.com>
2023-03-16 21:05:05 +0530

    First Commit

---
+++ src/main/java/org/bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package org.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("---Welcome To Indian Census Analyser Program---");
+    }
+}
